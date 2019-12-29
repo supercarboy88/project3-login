84c23238b0d506e7d40d035025bda2f8820cd885
YI YING <supercarboy88@gmail.com>
2019-12-29 04:40:11 -0800

    ADD

---
+++ src/components/home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    constructor() {
+        super()
+    }
+
+
+    render() {
+        const imageStyle = {
+            width: 400
+        }
+        return (
+            <div>
+                <p>Welcome to our Finance App</p>
+            </div>
+        )
+
+    }
+}
+
+export default Home
